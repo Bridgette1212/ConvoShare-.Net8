aa475022e91a7bb2d6e988ba6d3d6d05c9f6f8de
Mologadi Maboya <Mologadi.Maboya@hollywoodbets.net>
2024-07-15 22:41:11 +0200

    Added User API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController: ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+
+        var users = await  _context.Users.ToListAsync();
+        return users;
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int Id){
+         var user = await _context.Users.FindAsync(Id);
+         if(user == null) return NotFound();
+        return user;
+    }
+}
